4bf43ad16c7d3c877348a9d1552c8585f32dda69
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-02-11 09:54:29 -0500

    Add a Greetings Function

---
+++ Functions-GIT.py
@@ -0,0 +1,11 @@
+#4.13.3: Greetings
+#Jacob Shoup 2/6/19
+
+print("")
+name = input("( o‿o) Hey what is your name: ")
+
+def greeting():
+    print("(o‿o) Hi there non-gender specific person, what is your " + name + "?")
+    print("(OヮO) Nice to meet you!")
+#
+greeting()
