7004918ff10e3c021ea4db1102b25d08e48f2127
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-02-14 09:33:46 -0500

    Add Functions & Variables

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -1,11 +1,26 @@
 #4.13.3: Greetings
 #Jacob Shoup 2/6/19
 
+'''
 print("")
 name = input("( o‿o) Hey what is your name: ")
 
 def greeting():
     print("(o‿o) Hi there non-gender specific person, what is your " + name + "?")
     print("(OヮO) Nice to meet you!")
-#
+
 greeting()
+'''
+
+# 4.13.3: Functions and Variables
+# Jacob Shoup
+# 2.14.19
+
+x = 420
+
+def print_something():
+    x = 13
+    print(x)
+
+print_something()
+print(x)
