34d4db267f9e58c7f83c6f5d0aa1a2d83acf36ee
Jacob Shoup <02jacob.shoup@gulllakecs.org>
2019-02-27 09:59:30 -0500

    Functions and Variables, Part 3

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -1,7 +1,7 @@
 #4.13.3: Greetings
 #Jacob Shoup 2/6/19
 
-'''
+
 print("")
 name = input("( o‿o) Hey what is your name: ")
 
@@ -10,7 +10,7 @@ def greeting():
     print("(OヮO) Nice to meet you!")
 
 greeting()
-'''
+
 
 # 4.13.3: Functions and Variables
 # Jacob Shoup
@@ -24,3 +24,28 @@ def print_something():
 
 print_something()
 print(x)
+
+
+# 4.13.5: Functions & Variables - Part 2
+# Jacob Shoup
+# 2.14.19
+
+my_variable = 69.420
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+
+
+# 4.13.6: Functions & Variables, Part 3
+# Jacob Shoup
+# 2.18.19
+
+
+def print_number(x):
+    print(str(x))
+
+print_number(12)
+print_number('\n' + 'Hello World')
