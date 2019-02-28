8898f2d2fcdf00fd66326b6d744d62b78596e4c8
JacobShoup <48022398+JacobShoup@users.noreply.github.com>
2019-02-28 09:16:25 -0500

    Add Print Multiple Times

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -72,3 +72,16 @@ def print_two_numbers(x, y = 20):
 print_two_numbers(5, 67)
 print_two_numbers(23)
 
+# 4.14.7: Print Multple Times
+# Mr. Lange
+# 2.19.19
+
+def print_multiply_times(string, times):
+    for i in range(times):
+        print(string)
+
+print_multiply_times('Hello Computer scientist', 4)
+
+
+
+
